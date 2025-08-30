d402cdf2b5133a406268e8baa70075764a8c8b45
happyfloatingsquid <74300044+ivyrecords11@users.noreply.github.com>
2025-08-30 17:42:34 +0900

    Revert "Scripts 구조 만들어놨습니다"
    This reverts commit 15711c2c2619e2244d2d19dbc9721e43b967318f.

--- Assets/Scripts/Data/WeatherType.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Weathertype", menuName = "Scriptable Objects/Weathertype")]
-// WeatherType.cs
-
-public enum WeatherType { Heat, Rain, Snow, Wind }
