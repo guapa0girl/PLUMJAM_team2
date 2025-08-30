15711c2c2619e2244d2d19dbc9721e43b967318f
happyfloatingsquid <74300044+ivyrecords11@users.noreply.github.com>
2025-08-30 17:29:00 +0900

    Scripts 구조 만들어놨습니다
    상세는 게임 기획안이랑 대조 및 구체화하면서 수정할 예정입니다

---
+++ Assets/Scripts/Data/WeatherType.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Weathertype", menuName = "Scriptable Objects/Weathertype")]
+// WeatherType.cs
+
+public enum WeatherType { Heat, Rain, Snow, Wind }
